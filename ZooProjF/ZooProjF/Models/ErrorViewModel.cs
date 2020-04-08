bbda65591302e0030c51c48bd8f09994c27a810b
Thanh Le <letanthanh95@gmail.com>
2020-04-08 01:29:30 -0500

    new patch

---
+++ ZooProjF/ZooProjF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZooProjF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
