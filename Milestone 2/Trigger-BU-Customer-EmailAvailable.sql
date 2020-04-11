01594d77c443012865e7d85446b2bd3f85753409
Teh-Mad-Cow <5084133+Teh-Mad-Cow@users.noreply.github.com>
2020-04-10 19:58:58 -0500

    Corrected trigger for Customer Update to only reject an update to the row if the Email changed AND is already in the DB

--- Milestone 2/Trigger-BU-Customer-EmailAvailable.sql
+++ Milestone 2/Trigger-BU-Customer-EmailAvailable.sql
@@ -2,7 +2,7 @@ CREATE DEFINER = CURRENT_USER TRIGGER `Team10`.`Customer_BEFORE_UPDATE` BEFORE U
 # If you change the code below, reflect the changes in Customer_BEFORE_INSERT and Customer_BEFORE_UPDATE
 BEGIN
 	# Verifies there are 0 matches for email in DB
-	IF 0 <> (SELECT COUNT(*) FROM `Team10`.`Customer` AS C WHERE C.Email = NEW.Email LIMIT 1) THEN
+	IF 0 <> (SELECT COUNT(*) FROM `Team10`.`Customer` AS C WHERE C.Email = NEW.Email AND C.Customer_ID <> NEW.Customer_ID LIMIT 1) THEN
 		# throws an error and cancels the insertion
 		SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = 'That email address is already in use, please select another email.';
